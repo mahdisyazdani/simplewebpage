7da579578bb6bdc16ae1a9ede300f8e7d304e238
mahdisyazdani <mahdis_26h@yahoo.com>
2016-05-15 12:38:47 +0430

    search box, debug

--- test1-javascript.js
+++ test1-javascript.js
@@ -1,15 +1,12 @@
-//variables
-var searchIcon = document.getElementsByClassName("search")[0];
-var searchBox = document.getElementsByClassName("search-box")[0]
-var home = document.getElementsByClassName("home")[0];
-var newsContent = document.getElementById("news");
-var content = document.getElementsByClassName("content")[0];
-var news = document.getElementsByClassName("news")[0];
-//show news in the main page
-function showNews() {
-    content.style.display = "none";
-    newsContent.style.display = "block";
-}
+var searchIcon = $(".search");
+var inputBox = $(".search-box")[0];
+var isOpen = false;
+
+var news = $(".news")[0];
+var newsContent = $("#news")[0];
+var content = $(".content")[0];
+var home = $(".home")[0];
+
 //read news from json
 function readNews(array) {
     var leftmenuinnerinner = document.getElementsByClassName("leftmenuinnerinner")[0];
@@ -23,38 +20,67 @@ function readNews(array) {
     newsContent.innerHTML = out;
     leftmenuinnerinner.innerHTML += newsTitleList;
 }
-//open search input when mouse over
-function showSearchBox() {
-    searchIcon.style.marginLeft = "0px";
-    searchBox.style.display = "inline-block";
-    isClicked = true;
-}
-//hide search input when mouse out
-function hideSearchBox() {
-    searchIcon.style.marginLeft = "15px";
-    searchBox.style.display = "none";
-    isClicked = false;
-}
-//show main content
-function showHome() {
-    content.style.display = "block";
-    newsContent.style.display = "none";
-}
-
-searchIcon.addEventListener("mouseover", showSearchBox);
-searchBox.addEventListener("mouseout", hideSearchBox);
-home.addEventListener("click", showHome);
-news.addEventListener("click", showNews);
-showHome();
-
 $(document).ready(function () {
+    //init
+    init();
+    //go to top of the selected news from menu
+    goToSelectedNews();
+    //search box setting
+    searchBoxSetting()
+    //show news in the main page
+    showNews()
+    //show main conten
+    showHome()
+});
+function init() {
+    $(content).css("display","block");
+    $(newsContent).css("display","none");
+}
+function showNews() {
+    $(news).click(function () {
+        $(content).css("display","none");
+        $(newsContent).css("display","block");
+    });
+}
+function showHome(){
+    $(home).click(function () {
+        $(content).css("display","block");
+        $(newsContent).css("display","none");
+    });
+}
+function searchBoxSetting(){
+    searchIcon.mouseover(function () {
+        if (!isOpen){
+            $(inputBox).animate({width:'200px',marginLeft:'15px'},"slow",function () {
+                isOpen = true;
+                $(this).find('input')[0].focus();
+            });
+        }
+    });
+    $(document).mouseup(function (e) {
+        if (!searchIcon.is(e.target) && searchIcon.has(e.target).length == 0){
+            if (isOpen){
+                $(inputBox).animate({width:'0px',marginLeft:'0px'},"slow",function () {
+                    isOpen = false;
+                });
+            }
+        }
+
+    });
+}
+function goToSelectedNews() {
+
     $(".leftmenuinnerinner").click(function (event) {
+        if($(content).css("display")=="block"){
+            $(content).css("display","none");
+            $(newsContent).css("display","block");
+        }
         event.preventDefault();
         var id =  $(event.target).attr("id");
         var contentLink = $(".news-item")[id-1];
         var selectedDivId = "#"+$(contentLink).attr("id");
         $('html, body').animate({
-            scrollTop: $(selectedDivId).offset().top /*- $(window).scrollTop()*/
+            scrollTop: $(selectedDivId).offset().top
         }, 2000);
     });
-});
+}
